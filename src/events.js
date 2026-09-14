0ee89e7421f562330eea2c98679c2e8b38ef4ed9
ATechguy124 <noorlun.matthew.30@c-dh.org>
2026-09-13 19:57:15 -0500

    Update events.js

--- src/events.js
+++ src/events.js
@@ -1,37 +1,66 @@
 export class EventManager {
-  constructor(canvas, onClickCallback) {
+  constructor(canvas, onClickCallback, onRedrawCallback) {
     this.canvas = canvas;
     this.onClick = onClickCallback;
+    this.onRedraw = onRedrawCallback;
     this.currentRenderList = [];
+    this.dragTarget = null;
+    this.dragOffset = { x: 0, y: 0 };
 
-    this.canvas.addEventListener('click', (e) => this.handleClick(e));
+    this.canvas.addEventListener('mousedown', (e) => this.handleMouseDown(e));
+    this.canvas.addEventListener('mousemove', (e) => this.handleMouseMove(e));
+    window.addEventListener('mouseup', () => this.handleMouseUp());
   }
 
-  handleClick(event) {
+  getCanvasCoords(e) {
     const rect = this.canvas.getBoundingClientRect();
-    const scaleX = this.canvas.width / rect.width;
-    const scaleY = this.canvas.height / rect.height;
+    return {
+      x: (e.clientX - rect.left) * (this.canvas.width / rect.width),
+      y: (e.clientY - rect.top) * (this.canvas.height / rect.height)
+    };
+  }
 
-    const clickX = (event.clientX - rect.left) * scaleX;
-    const clickY = (event.clientY - rect.top) * scaleY;
+  handleMouseDown(e) {
+    const { x, y } = this.getCanvasCoords(e);
 
-    // Check top-most visible elements first
     for (let i = this.currentRenderList.length - 1; i >= 0; i--) {
       const el = this.currentRenderList[i];
       if (el.hidden) continue;
 
-      if (el.type === 'button' || el.type === 'box') {
-        if (
-          clickX >= el.x &&
-          clickX <= el.x + el.w &&
-          clickY >= el.y &&
-          clickY <= el.y + el.h
-        ) {
-          this.onClick(el.id);
-          break;
+      const width = el.w || 100;
+      const height = el.h || 30;
+
+      if (x >= el.x && x <= el.x + width && y >= el.y && y <= el.y + height) {
+        // Bring element to the front of the render stack (Z-index layering)
+        this.currentRenderList.splice(i, 1);
+        this.currentRenderList.push(el);
+
+        if (el.draggable) {
+          this.dragTarget = el;
+          this.dragOffset.x = x - el.x;
+          this.dragOffset.y = y - el.y;
         }
+
+        if (el.type === 'button') {
+          this.onClick(el.id);
+        }
+
+        this.onRedraw();
+        break;
       }
     }
+  }
+
+  handleMouseMove(e) {
+    if (!this.dragTarget) return;
+    const { x, y } = this.getCanvasCoords(e);
+    this.dragTarget.x = x - this.dragOffset.x;
+    this.dragTarget.y = y - this.dragOffset.y;
+    this.onRedraw();
+  }
+
+  handleMouseUp() {
+    this.dragTarget = null;
   }
 
   updateRenderList(elements) {
